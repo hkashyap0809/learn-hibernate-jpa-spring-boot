5da1838edbd41fd74e6bc539d69f40eb23c2efd6
hkashyap0809 <hkashyap0809@gmail.com>
2023-09-29 17:15:33 +0530

    setup-h2
    setup h2 console and populate data queries

---
+++ database-demo/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+create table person ( 
+	id			integer 		not null, 
+	name	 	varchar(255) 	not null, 
+	location	varchar(255), 
+	birth_date	timestamp,
+	primary key(id)
+);
+
+insert into person ( ID,NAME,LOCATION,BIRTH_DATE) 
+values(1001, 'Harshit Kashyap', 'Dehradun', '1995-09-08 13:10:11');
+
+insert into person ( ID,NAME,LOCATION,BIRTH_DATE) 
+values(1002, 'Anjaneya', 'Allahabad', '1996-05-14 13:10:11');
+
+insert into person ( ID,NAME,LOCATION,BIRTH_DATE) 
+values(1003, 'Nagu Bhai', 'Alahabad', '1996-02-10 13:10:11');
