0c42c891c806d191cd38f0cc9ccc6d79238b13e9
hkashyap0809 <hkashyap0809@gmail.com>
2023-09-29 19:21:31 +0530

    insert-and-update
    implementation of insert and delete through Spring JDBC

--- database-demo/src/main/java/com/learn/database/databasedemo/DatabaseDemoApplication.java
+++ database-demo/src/main/java/com/learn/database/databasedemo/DatabaseDemoApplication.java
@@ -1,5 +1,7 @@
 package com.learn.database.databasedemo;
 
+import java.sql.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.learn.database.databasedemo.entity.Person;
 import com.learn.database.databasedemo.jdbc.PersonJdbcDao;
 
 
@@ -27,6 +30,11 @@ public class DatabaseDemoApplication implements CommandLineRunner{
 		logger.info("All Persons -> {} ", personJdbcDao.findAll());
 		logger.info("User ID 1003 -> {} ", personJdbcDao.findById(1003));
 		logger.info("Deleting ID 1002 -> {} ", personJdbcDao.deleteById(1002));
+		logger.info("All Persons -> {} ", personJdbcDao.findAll());
+		logger.info("All Persons -> {} ", personJdbcDao.findAll());
+		logger.info("Insert 1004 -> {}", personJdbcDao.insert(new Person(1004, "Rachita","Hyderabad",new Date(0))));
+		logger.info("All Persons -> {} ", personJdbcDao.findAll());
+		logger.info("Update 1004 -> {}", personJdbcDao.update(new Person(1004, "Rachita Bangali","Hyderabad",new Date(0))));
 		logger.info("All Persons -> {} ", personJdbcDao.findAll());
 	}
 
--- database-demo/src/main/java/com/learn/database/databasedemo/jdbc/PersonJdbcDao.java
+++ database-demo/src/main/java/com/learn/database/databasedemo/jdbc/PersonJdbcDao.java
@@ -1,5 +1,6 @@
 package com.learn.database.databasedemo.jdbc;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,32 @@ public class PersonJdbcDao {
 	public int deleteById(int id){
 		return jdbcTemplate
 				.update("delete from person where id=?", new Object[]{id});
+	}
+	
+	public int insert(Person person){
+		return jdbcTemplate
+				.update("insert into person(id,name,location,birth_date) "
+						
+						+"values (?,?,?,?)", 
+						new Object[]{
+								person.getId(),
+								person.getName(),
+								person.getLocation(),
+								new Timestamp(person.getBirthDate().getTime())}
+						);
+	}
+	
+	public int update(Person person){
+		return jdbcTemplate
+				.update("update person "
+						+" set name = ?, location = ? , birth_date = ? "	
+						+" where id = ?", 
+						new Object[]{
+								person.getName(),
+								person.getLocation(),
+								new Timestamp(person.getBirthDate().getTime()),
+								person.getId()}
+						);
 	}
 
 
