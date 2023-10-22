42e738d722c048fe03ad1a08d9f01c0e9c238a51
hkashyap0809 <hkashyap0809@gmail.com>
2023-10-22 15:27:18 +0530

    joined
    joined inheritance strategy

--- jpa-hibernate-demo/src/main/java/com/learn/jpa/hibernate/jpahibernatedemo/entity/Employee.java
+++ jpa-hibernate-demo/src/main/java/com/learn/jpa/hibernate/jpahibernatedemo/entity/Employee.java
@@ -10,7 +10,7 @@ import jakarta.persistence.InheritanceType;
 
 
 @Entity
-@Inheritance( strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance( strategy = InheritanceType.JOINED)
 public abstract class Employee {
 
 	@Id
