b3085d4ecf158dc02fa0ad8ece41145d5468afd4
hkashyap0809 <hkashyap0809@gmail.com>
2023-10-22 14:57:09 +0530

    single-table-method
    single table method and discriminator column

--- jpa-hibernate-demo/src/main/java/com/learn/jpa/hibernate/jpahibernatedemo/entity/Employee.java
+++ jpa-hibernate-demo/src/main/java/com/learn/jpa/hibernate/jpahibernatedemo/entity/Employee.java
@@ -2,12 +2,17 @@ package com.learn.jpa.hibernate.jpahibernatedemo.entity;
 
 
 import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 
 
 @Entity
+@Inheritance( strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "EmployeeType")
 public abstract class Employee {
 
 	@Id
